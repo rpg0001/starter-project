1a0387bc9df31278224fd0c6d236a54d9b2fba69
Robyn <77750040+rpg0001@users.noreply.github.com>
2024-09-07 18:16:44 +0100

    jsonapi errors

--- api/src/controllers/notesController.ts
+++ api/src/controllers/notesController.ts
@@ -1,26 +1,19 @@
 import * as NotesModel from '../models/notesModel';
+import { getBadRequestError, getInternalServerError, getNotFoundError } from '../utils/errors';
 
 export async function getNote(req: any, res: any) {
     const id = Number(req.params.id);
     try {
-        if (isNaN(id)) {
-            const message = `Bad request - Id (${id}) must be a number`;
-            console.error(message);
-            return res.status(400).json(message);
-        }
-    
+        if (isNaN(id)) return res.status(400).json(getBadRequestError('/id'));
+
         const note = await NotesModel.getNote(req.params.id);
-        
-        if (!note) {
-            const message = `Not found - No note found matching id ${id}`;
-            console.error(message);
-            return res.status(404).json();
-        }
+
+        if (!note) return res.status(404).json(getNotFoundError());
 
         return res.status(200).json(note);
     } catch (error: any) {
         console.error(`Error retrieving note with id ${id}: ${error.message}`);
-        return res.status(500).json();
+        return res.status(500).json(getInternalServerError());
     }
 }
 
@@ -39,60 +32,43 @@ export async function createNote(req: any, res: any) {
         const title = req.body?.title;
         const content = req.body?.content;
 
-        if (!title || !content) {
-            const message = `Bad request - title or content not provided`;
-            console.error(message);
-            return res.status(400).json(message);
-        }
+        if (!title) return res.status(400).json(getBadRequestError('/title'));
+        if (!content) return res.status(400).json(getBadRequestError('/content'));
 
         const note = await NotesModel.createNote(title, content);
         return res.status(201).json(note);
     } catch (error: any) {
         console.error(`Error creating note: ${error.message}`);
-        return res.status(500).json();
+        return res.status(500).json(getInternalServerError());
     }
 }
 
 export async function updateNote(req: any, res: any) {
     const id = Number(req.params.id);
     try {
-        if (isNaN(id)) {
-            const message = `Bad request - Id (${id}) must be a number`;
-            console.error(message);
-            return res.status(400).json(message);
-        }
-
         const title = req.body?.title;
         const content = req.body?.content;
 
-        if (!title && !content) {
-            const message = `Bad request - title and content not provided`;
-            console.error(message);
-            return res.status(400).json(message);
-        }
-
+        if (isNaN(id)) return res.status(400).json(getBadRequestError('/id'));
+        if (!title && !content) return res.status(400).json(getBadRequestError('/body'));
+        
         const note = await NotesModel.updateNote(id, title, content);
         return res.status(200).json(note);
     } catch (error: any) {
         console.error(`Error updaing note with id ${id}: ${error.message}`);
-        return res.status(500).json();
+        return res.status(500).json(getInternalServerError());
     }
 }
 
 export async function deleteNote(req: any, res: any) {
     const id = Number(req.params.id);
     try {
-        if (isNaN(id)) {
-            const message = `Bad request - Id (${id}) must be a number`;
-            console.error(message);
-            return res.status(400).json(message);
-        }
-    
+        if (isNaN(id)) return res.status(400).json(getBadRequestError('/id'));
+        
         await NotesModel.deleteNote(req.params.id);
-
         return res.status(204).json();
     } catch (error: any) {
         console.error(`Error retrieving note with id ${id}: ${error.message}`);
-        return res.status(500).json();
+        return res.status(500).json(getInternalServerError());
     }
 }
---
+++ api/src/utils/errors.ts
@@ -0,0 +1,66 @@
+type JsonApiErrorResponse = {
+    errors: JsonApiError[]
+}
+
+type JsonApiError = {
+    status: number;
+    code: string;
+    source?: {
+        pointer?: string;
+        parameter?: string;
+        header?: string;
+    },
+    meta: {
+        timestamp: string;
+    }
+}
+
+function getJsonApiErrorResponse(
+    errors: JsonApiError[]
+): JsonApiErrorResponse {
+    return {
+        errors: errors
+    }
+}
+
+export function getInternalServerError(): JsonApiErrorResponse {
+    const error: JsonApiError = {
+        status: 500,
+        code: 'INTERNAL_SERVER_ERROR',
+        meta: {
+            timestamp: new Date().toISOString()
+        }
+    }
+    return getJsonApiErrorResponse([error]);
+}
+
+export function getBadRequestError(
+    pointer?: string, 
+    parameter?: string,
+    header?: string
+): JsonApiErrorResponse {
+    const error: JsonApiError = {
+        status: 400,
+        code: 'BAD_REQUEST',
+        source: {
+            pointer: pointer,
+            parameter: parameter,
+            header: header
+        },
+        meta: {
+            timestamp: new Date().toISOString()
+        }
+    }
+    return getJsonApiErrorResponse([error]);
+}
+
+export function getNotFoundError() {
+    const error: JsonApiError = {
+        status: 404,
+        code: 'NOT_FOUND',
+        meta: {
+            timestamp: new Date().toISOString()
+        }
+    }
+    return getJsonApiErrorResponse([error]);
+}
