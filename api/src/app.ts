54b413ee85a3a0034477755bc714c9da4e2235ba
Robyn <77750040+rpg0001@users.noreply.github.com>
2024-09-07 18:55:00 +0100

    customise morgan

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,15 +5,27 @@ import NotesRouter from './routers/notesRouter';
 const app = express();
 const port = 8080;
 
-app.use(morgan('tiny'));
+// Middleware
+app.use(morgan(function (tokens, req, res) {
+  return [
+    ['[',tokens.date(req, res),']'].join(''),
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    ['(',tokens['response-time'](req, res), 'ms)'].join('')
+  ].join(' ')
+}));
 app.use(express.json())
 
+// Health check
 app.get('/', (req, res) => {
-  res.status(200).send("OK");
+  res.status(200).send('OK');
 });
 
+// Routers
 app.use(NotesRouter);
 
+// Start server
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
