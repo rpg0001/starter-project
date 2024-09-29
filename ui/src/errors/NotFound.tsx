2f55d4a905f66ecb572f8d76b1e767b42c58226c
Robyn <77750040+rpg0001@users.noreply.github.com>
2024-09-29 11:10:52 +0100

    (ui) edit and delete buttons, 404 page css

--- ui/src/errors/NotFound.tsx
+++ ui/src/errors/NotFound.tsx
@@ -1,7 +1,8 @@
 export default function NotFound() {
     return (
         <div>
-            That page doesn't seem to exist!
+            <p className='text-orange-500 text-2xl font-bold'>404</p>
+            <p>That page doesn't seem to exist!</p>
         </div>
     )
 }
--- ui/src/notes/NoteDetails.tsx
+++ ui/src/notes/NoteDetails.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getNote, Note } from "../services/notes";
 import './Notes.css';
@@ -17,10 +17,17 @@ export default function NoteDetails() {
         <div>
             <h1>View note</h1>
             {note ?
-                <div className='note'>
-                    <h2>{note?.attributes.title}</h2>
-                    <p>{note?.attributes.content}</p>
-                </div>
+                <>
+                    <div className='note'>
+                        <h2>{note?.attributes.title}</h2>
+                        <p>{note?.attributes.content}</p>
+                    </div>
+                    <div className='flex flex-row gap-2'>
+                        <Link to={`${note.id}/edit`} >edit</Link>
+                        |
+                        <Link to={`${note.id}/delete`} >delete</Link>
+                    </div>
+                </>
             :
                 <div>
                     <p>Failed to retrieve note</p>
