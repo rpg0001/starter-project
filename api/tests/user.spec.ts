e0c8f3c785dd285ecb93ddc07e0339f7beca31eb
Robyn <77750040+rpg0001@users.noreply.github.com>
2024-12-08 17:48:34 +0000

    (api) user playwright tests

---
+++ api/tests/user.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test';
+
+test('should create, fetch and delete a user', async ({ request }) => {
+  /* Create new user */
+
+  // Arrange
+  const createUserRequestBody = {
+      "email": "test_user@example.com",
+      "username": "test_user"
+  }
+
+  // Act
+  const createUserResponse = await request.post("/users", { "data": createUserRequestBody });
+
+  // Assert
+  expect(createUserResponse.status()).toBe(201);
+  const createUserResponseBody = await createUserResponse.json();
+  console.log(createUserResponse);
+  console.log(createUserResponseBody);
+  expect(Number(createUserResponseBody.id)).not.toBeNaN();
+  expect(createUserResponseBody.email).toBe(createUserRequestBody.email);
+  expect(createUserResponseBody.username).toBe(createUserRequestBody.username);
+
+
+  /* Fetch new user */
+
+  // Arrange
+  const userId = Number(createUserResponseBody.id);
+
+  // Act
+  const getUserResponse = await request.get(`/users/${userId}`);
+
+  // Assert
+  expect(getUserResponse.status()).toBe(200);
+  const getUserResponseBody = await getUserResponse.json();
+  console.log(getUserResponse);
+  console.log(getUserResponseBody);
+  expect(Number(getUserResponseBody.id)).toBe(userId);
+  expect(getUserResponseBody.email).toBe(createUserRequestBody.email);
+  expect(getUserResponseBody.username).toBe(createUserRequestBody.username);
+
+
+  /* Delete new user */
+
+  // Arrange
+  // Act
+  const deleteUserResponse = await request.delete(`/users/${userId}`);
+
+  // Assert
+  expect(deleteUserResponse.status()).toBe(204);
+})
