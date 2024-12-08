2250261286dcf5877639e73ee3ac5a4e3b5ab3fa
Robyn <77750040+rpg0001@users.noreply.github.com>
2024-12-08 17:53:21 +0000

    (actions) remove browser install, run webserver

--- api/playwright.config.ts
+++ api/playwright.config.ts
@@ -32,9 +32,9 @@ export default defineConfig({
   },
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: `http://127.0.0.1:${process.env.PORT ?? 8080}`,
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: `http://127.0.0.1:${process.env.PORT ?? 8080}`,
+    reuseExistingServer: !process.env.CI,
+  },
 });
